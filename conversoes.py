64389c14dfc0dd920731691d50e404952ba2747d
Luís Fernando Nazaré Silva <nazareth.s.fernando@gmail.com>
2024-09-16 17:38:00 -0300

    Update conversoes.py

--- conversoes.py
+++ conversoes.py
@@ -21,3 +21,53 @@ def converter_temperatura(valor, de, para):
         return "Unidade inválida."
     except Exception as e:
         return f"Ocorreu um erro: {e}"
+
+
+def converter_distancia(valor, de, para):
+    """
+    Converte distância entre metros, quilômetros e milhas.
+    """
+    try:
+        if de == 'M':
+            if para == 'KM':
+                return valor / 1000
+            elif para == 'MI':
+                return valor * 0.000621371
+        elif de == 'KM':
+            if para == 'M':
+                return valor * 1000
+            elif para == 'MI':
+                return valor * 0.621371
+        elif de == 'MI':
+            if para == 'M':
+                return valor / 0.000621371
+            elif para == 'KM':
+                return valor / 0.621371
+        return "Unidade inválida."
+    except Exception as e:
+        return f"Ocorreu um erro: {e}"
+
+
+def converter_peso(valor, de, para):
+    """
+    Converte peso entre gramas, quilogramas e libras.
+    """
+    try:
+        if de == 'G':
+            if para == 'KG':
+                return valor / 1000
+            elif para == 'LB':
+                return valor * 0.00220462
+        elif de == 'KG':
+            if para == 'G':
+                return valor * 1000
+            elif para == 'LB':
+                return valor * 2.20462
+        elif de == 'LB':
+            if para == 'G':
+                return valor / 0.00220462
+            elif para == 'KG':
+                return valor / 2.20462
+        return "Unidade inválida."
+    except Exception as e:
+        return f"Ocorreu um erro: {e}"
